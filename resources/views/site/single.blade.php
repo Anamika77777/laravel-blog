c6d2409eede8cf8ca54002516c6c4dd5630e25ad
Anamika77777 <anamikasen8906@gmail.com>
2024-07-26 10:32:34 +0530

    changes in comment section

--- resources/views/site/single.blade.php
+++ resources/views/site/single.blade.php
@@ -49,96 +49,13 @@
                                        @foreach ($blog->tags as $tag)
                                        <li class="list-inline-item"><a href="#" rel="tag">{{$tag->name}}</a></li>
                                        @endforeach
-
-                                        
-                                       
-                                    </ul>
-
-                                    <ul class="list-inline">
-                                        <li class="list-inline-item"> Share: </li>
-                                        <li class="list-inline-item"><a href="#" ><i class="fab fa-facebook-f"
-                                                    aria-hidden="true"></i></a></li>
-                                        <li class="list-inline-item"><a href="#" ><i class="fab fa-twitter"
-                                                    aria-hidden="true"></i></a></li>
-                                        <li class="list-inline-item"><a href="#" ><i class="fab fa-pinterest-p"
-                                                    aria-hidden="true"></i></a></li>
-                                        <li class="list-inline-item"><a href="#" ><i class="fab fa-google-plus"
-                                                    aria-hidden="true"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-
                     <div class="col-lg-12 mb-5">
-                        <div class="posts-nav bg-white p-5 d-lg-flex d-md-flex justify-content-between ">
-                            <a class="post-prev align-items-center" href="#">
-                                <div class="posts-prev-item mb-4 mb-lg-0">
-                                    <span class="nav-posts-desc text-color">- Previous Post</span>
-                                    <h6 class="nav-posts-title mt-1">
-                                        Donec consectetuer ligula <br>vulputate sem tristique.
-                                    </h6>
-                                </div>
-                            </a>
-                            <div class="border"></div>
-                            <a class="posts-next" href="#">
-                                <div class="posts-next-item pt-4 pt-lg-0">
-                                    <span class="nav-posts-desc text-lg-right text-md-right text-color d-block">- Next Post</span>
-                                    <h6 class="nav-posts-title mt-1">
-                                        Ut aliquam sollicitudin leo.
-                                    </h6>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-12 mb-5">
-                        <div class="comment-area card border-0 p-5">
-                            <h4 class="mb-4">2 Comments</h4>
-                            <ul class="comment-tree list-unstyled">
-                                <li class="mb-5">
-                                    <div class="comment-area-box">
-                                        <img loading="lazy" alt="comment-author" src="images/blog/test1.jpg" class="img-fluid float-left mr-3 mt-2">
-
-                                        <h5 class="mb-1">Philip W</h5>
-                                        <span>United Kingdom</span>
-
-                                        <div class="comment-meta mt-4 mt-lg-0 mt-md-0 float-lg-right float-md-right">
-                                            <a href="#"><i class="icofont-reply mr-2 text-muted"></i>Reply |</a>
-                                            <span class="date-comm">Posted October 7, 2018 </span>
-                                        </div>
-
-                                        <div class="comment-content mt-3">
-                                            <p>Some consultants are employed indirectly by the client via a consultancy staffing company, a
-                                                company that provides consultants on an agency basis. </p>
-                                        </div>
-                                    </div>
-                                </li>
-
-                                <li>
-                                    <div class="comment-area-box">
-                                        <img loading="lazy" alt="comment-author" src="images/blog/test2.jpg" class="mt-2 img-fluid float-left mr-3">
-
-                                        <h5 class="mb-1">Philip W</h5>
-                                        <span>United Kingdom</span>
-
-                                        <div class="comment-meta mt-4 mt-lg-0 mt-md-0 float-lg-right float-md-right">
-                                            <a href="#"><i class="icofont-reply mr-2 text-muted"></i>Reply |</a>
-                                            <span class="date-comm">Posted October 7, 2018</span>
-                                        </div>
-
-                                        <div class="comment-content mt-3">
-                                            <p>Some consultants are employed indirectly by the client via a consultancy staffing company, a
-                                                company that provides consultants on an agency basis. </p>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-12">
                         <form class="contact-form bg-white rounded p-5" id="comment-form">
                             <h4 class="mb-4">Write a comment</h4>
                             <div class="row">
@@ -161,6 +78,31 @@
                             <input class="btn btn-main btn-round-full" type="submit" name="submit-contact" id="submit_contact"
                                 value="Submit Message">
                         </form>
+                    </div>
+                    <div class="col-lg-12 mb-5">
+                        <div class="comment-area card border-0 p-5">
+                            <h4 class="mb-4">2 Comments</h4>
+                            <ul class="comment-tree list-unstyled">
+                                <li class="mb-5">
+                                    <div class="comment-area-box">
+                                        <img loading="lazy" alt="comment-author" src="{{asset('assets/site/images/blog/test1.jpg')}}" class="img-fluid float-left mr-3 mt-2">
+
+                                        <h5 class="mb-1">Philip W</h5>
+                                        <span>United Kingdom</span>
+
+                                        <div class="comment-meta mt-4 mt-lg-0 mt-md-0 float-lg-right float-md-right">
+                                            <a href="#"><i class="icofont-reply mr-2 text-muted"></i>Reply |</a>
+                                            <span class="date-comm">Posted October 7, 2018 </span>
+                                        </div>
+
+                                        <div class="comment-content mt-3">
+                                            <p>Some consultants are employed indirectly by the client via a consultancy staffing company, a
+                                                company that provides consultants on an agency basis. </p>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
