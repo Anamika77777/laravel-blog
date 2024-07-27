55afe6ea39daf18c672c1b0290aab8c368cdae4a
Anamika77777 <anamikasen8906@gmail.com>
2024-07-27 22:27:13 +0530

    reply

--- resources/views/site/single.blade.php
+++ resources/views/site/single.blade.php
@@ -36,7 +36,7 @@
                                         <div class="blog-item-meta bg-gray pt-2 pb-1 px-3">
                                             <span class="text-black text-capitalize mr-3"><i class="ti-time mr-1"></i>
                                                 {{ date('d-M-y', strtotime($blog->created_at)) }}</span>
-                                            <span class="text-muted text-capitalize mr-3"><i class="ti-comment mr-2"></i>5
+                                            <span class="text-muted text-capitalize mr-3"><i class="ti-comment mr-2"></i>{{count($comments)}}
                                                 Comments</span>
                                         </div>
                                         <h2 class="mt-3 mb-4">{{ $blog->title }}</h2>
