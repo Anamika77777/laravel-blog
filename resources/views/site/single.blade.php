edf5b34daf96af8364561cf1a515718915e3c292
Anamika77777 <anamikasen8906@gmail.com>
2024-07-31 17:10:34 +0530

    done

--- resources/views/site/single.blade.php
+++ resources/views/site/single.blade.php
@@ -9,12 +9,12 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="block text-center">
-                        <span class="text-white">News details</span>
+                        <span class="text-white">Blog details</span>
                         <h1 class="text-capitalize mb-4 text-lg">Blog Single</h1>
                         <ul class="list-inline">
                             <li class="list-inline-item"><a href="{{ url('/') }}" class="text-white">Home</a></li>
                             <li class="list-inline-item"><span class="text-white">/</span></li>
-                            <li class="list-inline-item text-white-50">News details</li>
+                            <li class="list-inline-item text-white-50">Blog details</li>
                         </ul>
                     </div>
                 </div>
